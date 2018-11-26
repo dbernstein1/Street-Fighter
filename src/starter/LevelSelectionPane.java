6d751f6c82904c7c6b81bcb3bc615fb53eb2a606
Miguel Rojas <m_rojas5@u.pacific.edu>
2018-11-26 10:29:22 -0800

    rename packages for better access

--- src/starter/LevelSelectionPane.java
+++ src/starter/LevelSelectionPane.java
@@ -7,13 +7,14 @@ import acm.graphics.GObject;
 //level select
 public class LevelSelectionPane extends GraphicsPane {
 	private MainApplication program;
-	private static final String IMAGES[]= {"example_back.jpg","portMapMain.png","menu_back_arrow.png","highlight.png"};
-	private GImage arena1,arena2,backArrow,highlight;
+	private static final String IMAGES[]= {"example_back.jpg","portMapMain.png", "menu_back_arrow.png","highlight.png", "forestMap.jpg"};
+	private GImage arena1,arena2,arena3,backArrow,highlight;
 	private GButton fight;
 	private Background back_Ground;
 	private int choice=1;
 	private Level LEVEL_DANIEL=new Level(1,arena1);
 	private Level LEVEL_MIGUEL=new Level(2,arena2);
+	private Level LEVEL_MIGUEL1=new Level(3,arena3);
 	public LevelSelectionPane(MainApplication app )
 	{
 		this.program=app;
@@ -21,6 +22,8 @@ public class LevelSelectionPane extends GraphicsPane {
 		arena1.setSize(300, 200);
 		arena2=new GImage(IMAGES[1],700,100);
 		arena2.setSize(300, 200);
+		arena3=new GImage(IMAGES[4],100,400);
+		arena3.setSize(300, 200);
 		backArrow=new GImage(IMAGES[2],0,0);
 		backArrow.setSize(80,80);
 		fight=new GButton("Fight",435,265,200,100);
@@ -86,6 +89,20 @@ public class LevelSelectionPane extends GraphicsPane {
 			program.add(back_Ground.getImage());
 			showContents();
 		}
+		if(obj==arena3)
+		{
+			choice=3;
+			System.out.println(choice);
+			program.remove(back_Ground.getImage());
+			program.remove(highlight);
+		
+			highlight=new GImage(IMAGES[3],680,80);
+			highlight.setSize(340 ,240);
+			program.add(highlight);
+			back_Ground=program.backgroundPort;
+			program.add(back_Ground.getImage());
+			showContents();
+		}
 		if(obj==fight)
 		{	
 			checkChoice();
@@ -104,6 +121,7 @@ public class LevelSelectionPane extends GraphicsPane {
 			program.switchToCharacterPane();
 			break;
 		case 3:
+			program.getGamePane().set_Choice(LEVEL_MIGUEL1);
 			program.switchToCharacterPane();
 			break;
 		case 4:
