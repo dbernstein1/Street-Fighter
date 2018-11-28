f0f583c685250cfaef3c377b2a020889ba4ae010
aghantou <a_ghantous@u.pacific.edu>
2018-11-27 20:08:52 -0800

    Fixed player dropping from heights

--- src/starter/Animation.java
+++ src/starter/Animation.java
@@ -136,13 +136,18 @@ public class Animation{
 				curState = 0;
 				idle();
 			}
+			if (!player.isJumping) {
+				drop();
+			}
 		}
 		if(curState != prevState)
 			frame = 0;
 		prevState = curState;
 		frame++;
 		
-	}
+		}
+		
+	
 	
 	public void idle()
 	{
